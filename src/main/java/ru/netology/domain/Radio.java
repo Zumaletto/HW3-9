4778b84e13f6098eedc8cf4f24afad72f27c2da2
Kirillova Olesya <olesyak-85@mail.ru>
2021-09-12 09:47:29 +0300

    create maven.Added class Radio

---
+++ src/main/java/ru/netology/domain/Radio.java
@@ -0,0 +1,68 @@
+package ru.netology.domain;
+
+public class Radio {
+    private int minRadioWave = 0;
+    private int maxRadioWave = 9;
+    private int currentRadioWave;
+    private int minSoundValue = 0;
+    private int maxSoundValue = 10;
+    private int currentSoundValue;
+
+    public void setCurrentRadioWave(int currentRadioWave) {
+        if (currentRadioWave > maxRadioWave || currentRadioWave < minRadioWave) {
+            return;
+        }
+        this.currentRadioWave = currentRadioWave;
+    }
+
+    public int getCurrentRadioWave() {
+        return currentRadioWave;
+    }
+
+    public int prev(int currentRadioWave) {
+        if (currentRadioWave < maxRadioWave) {
+            currentRadioWave = currentRadioWave + 1;
+        } else {
+            currentRadioWave = minRadioWave;
+        }
+        return currentRadioWave;
+    }
+
+    public int next(int currentRadioWave) {
+        if (currentRadioWave > minRadioWave) {
+            currentRadioWave = currentRadioWave + 1;
+        } else {
+            currentRadioWave = maxRadioWave;
+        }
+        return currentRadioWave;
+    }
+
+    public void setCurrentSoundValue(int currentSoundValue) {
+        if (currentSoundValue < minSoundValue || currentSoundValue > maxSoundValue) {
+            return;
+        }
+        this.currentSoundValue = currentSoundValue;
+    }
+
+    public int getCurrentSoundValue() {
+        return currentSoundValue;
+    }
+
+    public int plus(int currentSoundValue) {
+        if (currentSoundValue < maxSoundValue) {
+            currentSoundValue = currentSoundValue + 1;
+        } else {
+            currentSoundValue = maxSoundValue;
+        }
+        return currentSoundValue;
+    }
+
+    public int minus(int currentSoundValue) {
+        if (currentSoundValue >= minSoundValue) {
+            currentSoundValue = currentSoundValue + 1;
+        } else {
+            currentSoundValue = minSoundValue;
+        }
+        return currentSoundValue;
+    }
+}
